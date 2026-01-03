2889c534e2acb809d01c34589b787fc1d9f61ca7
Divyanshu Mahajan <divyanshumahajan9191@gmail.com>
2026-01-03 18:55:09 +0530

    Add error handling middleware to routes

--- server/src/loaders/routes.js
+++ server/src/loaders/routes.js
@@ -1,5 +1,9 @@
+const errorHandler = require("../middlewares/errorHandler.middleware");
+
 module.exports = (app) => {
     app.get("/", (req,res) => {
         console.log("Routes Load successfully");
     });
+
+    app.use(errorHandler);
 }
