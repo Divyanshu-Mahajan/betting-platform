7949b18913e3d486d1153963d445be9739832e15
Divyanshu Mahajan <divyanshumahajan9191@gmail.com>
2026-01-03 18:47:41 +0530

    Implement ApiError class for structured error handling

---
+++ server/src/utils/api/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message);
+
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.isOperational = true;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+module.exports = ApiError;
